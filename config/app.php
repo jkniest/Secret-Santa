30c9db902dc89ad36d8f7254da1e6c62f4cb146e
Kniest Jordan <Kniest@basecom.de>
2017-11-03 19:36:33 +0100

    Cleanup

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Secret Santa'),
 
     /*
     |--------------------------------------------------------------------------
@@ -171,11 +171,7 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        App\Providers\AppServiceProvider::class
 
     ],
 
