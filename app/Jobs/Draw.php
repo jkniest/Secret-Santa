cd946fd118501c58da2d49cecf315608a05a0e3f
Kniest Jordan <Kniest@basecom.de>
2017-11-10 00:09:58 +0100

    Check if date and time is correct before triggering draw job

--- app/Jobs/Draw.php
+++ app/Jobs/Draw.php
@@ -7,6 +7,7 @@ use App\Discord\MessageService;
 use App\Models\Participant;
 use App\Models\State;
 use App\Stub;
+use Carbon\Carbon;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -33,6 +34,20 @@ class Draw
      */
     public function handle(MessageService $service)
     {
+        $month = config('santa.draw.month');
+        $day = config('santa.draw.day');
+        $hour = config('santa.draw.hour');
+
+        if ($month == null || $day == null || $hour == null) {
+            return;
+        }
+
+        $now = Carbon::now();
+        $isSameDay = $now->isSameDay(Carbon::createFromDate($now->year, $month, $day));
+        if (!$isSameDay || $now->hour != $hour || $now->minute !== 0) {
+            return;
+        }
+
         $participants = Participant::all();
 
         // Select random partners
--- tests/Unit/DrawTest.php
+++ tests/Unit/DrawTest.php
@@ -77,8 +77,223 @@ class DrawTest extends TestCase
         $this->assertEquals(1234, State::byName('announcement_id'));
     }
 
-    // TODO: if date is not set, dont do anything
-    // TODO: Validate date
+    /** @test */
+    public function if_the_date_is_not_set_dont_do_anything()
+    {
+        // Given: The state is set to DRAWING
+        State::set('bot', State::DRAWING);
+
+        // Given: The message service is faked
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement post id is 12345 and the channel id is 67890
+        State::set('announcement_id', 12345);
+        State::set('announcement_channel', 67890);
+
+        // Given: There are 10 participants
+        $this->create(Participant::class, [], 10);
+
+        // When: The draw job is called
+        dispatch(new Draw());
+
+        // Then: No participant should have a partner
+        $this->assertTrue(Participant::all()->every(function ($participant) {
+            return is_null($participant->partner_id);
+        }));
+
+        // Also: No direct messages should have been sent
+        $this->assertCount(0, $service->dmMessages);
+
+        // And: The old announcement post should not have been deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: No new announcement post should have been written
+        $this->assertEmpty($service->message);
+    }
+
+    /** @test */
+    public function if_the_month_is_wrong_dont_do_anything()
+    {
+        // Given: The date for drawing is the 10th of december, at 4pm
+        Config::set('santa.draw.month', 12);
+        Config::set('santa.draw.day', 10);
+        Config::set('santa.draw.hour', 16);
+
+        // Given: The current date is set to the 10th of november at 4pm
+        Carbon::setTestNow(Carbon::create(Carbon::now()->year, 11, 10, 16));
+
+        // Given: The state is set to DRAWING
+        State::set('bot', State::DRAWING);
+
+        // Given: The message service is faked
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement post id is 12345 and the channel id is 67890
+        State::set('announcement_id', 12345);
+        State::set('announcement_channel', 67890);
+
+        // Given: There are 10 participants
+        $this->create(Participant::class, [], 10);
+
+        // When: The draw job is called
+        dispatch(new Draw());
+
+        // Then: No participant should have a partner
+        $this->assertTrue(Participant::all()->every(function ($participant) {
+            return is_null($participant->partner_id);
+        }));
+
+        // Also: No direct messages should have been sent
+        $this->assertCount(0, $service->dmMessages);
+
+        // And: The old announcement post should not have been deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: No new announcement post should have been written
+        $this->assertEmpty($service->message);
+    }
+
+    /** @test */
+    public function if_the_day_is_wrong_dont_do_anything()
+    {
+        // Given: The date for drawing is the 10th of december, at 4pm
+        Config::set('santa.draw.month', 12);
+        Config::set('santa.draw.day', 10);
+        Config::set('santa.draw.hour', 16);
+
+        // Given: The current date is set to the 14th of december at 4pm
+        Carbon::setTestNow(Carbon::create(Carbon::now()->year, 12, 14, 16));
+
+        // Given: The state is set to DRAWING
+        State::set('bot', State::DRAWING);
+
+        // Given: The message service is faked
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement post id is 12345 and the channel id is 67890
+        State::set('announcement_id', 12345);
+        State::set('announcement_channel', 67890);
+
+        // Given: There are 10 participants
+        $this->create(Participant::class, [], 10);
+
+        // When: The draw job is called
+        dispatch(new Draw());
+
+        // Then: No participant should have a partner
+        $this->assertTrue(Participant::all()->every(function ($participant) {
+            return is_null($participant->partner_id);
+        }));
+
+        // Also: No direct messages should have been sent
+        $this->assertCount(0, $service->dmMessages);
+
+        // And: The old announcement post should not have been deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: No new announcement post should have been written
+        $this->assertEmpty($service->message);
+    }
+
+    /** @test */
+    public function if_the_hour_is_wrong_dont_do_anything()
+    {
+        // Given: The date for drawing is the 10th of december, at 4pm
+        Config::set('santa.draw.month', 12);
+        Config::set('santa.draw.day', 10);
+        Config::set('santa.draw.hour', 16);
+
+        // Given: The current date is set to the 10th of december at 8am
+        Carbon::setTestNow(Carbon::create(Carbon::now()->year, 12, 10, 8));
+
+        // Given: The state is set to DRAWING
+        State::set('bot', State::DRAWING);
+
+        // Given: The message service is faked
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement post id is 12345 and the channel id is 67890
+        State::set('announcement_id', 12345);
+        State::set('announcement_channel', 67890);
+
+        // Given: There are 10 participants
+        $this->create(Participant::class, [], 10);
+
+        // When: The draw job is called
+        dispatch(new Draw());
+
+        // Then: No participant should have a partner
+        $this->assertTrue(Participant::all()->every(function ($participant) {
+            return is_null($participant->partner_id);
+        }));
+
+        // Also: No direct messages should have been sent
+        $this->assertCount(0, $service->dmMessages);
+
+        // And: The old announcement post should not have been deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: No new announcement post should have been written
+        $this->assertEmpty($service->message);
+    }
+
+    /** @test */
+    public function if_the_minute_is_not_zero_dont_do_anything()
+    {
+        // Given: The date for drawing is the 10th of december, at 4pm
+        Config::set('santa.draw.month', 12);
+        Config::set('santa.draw.day', 10);
+        Config::set('santa.draw.hour', 16);
+
+        // Given: The current date is set to the 10th of december at 4:15pm
+        Carbon::setTestNow(Carbon::create(Carbon::now()->year, 12, 10, 16, 15));
+
+        // Given: The state is set to DRAWING
+        State::set('bot', State::DRAWING);
+
+        // Given: The message service is faked
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement post id is 12345 and the channel id is 67890
+        State::set('announcement_id', 12345);
+        State::set('announcement_channel', 67890);
+
+        // Given: There are 10 participants
+        $this->create(Participant::class, [], 10);
+
+        // When: The draw job is called
+        dispatch(new Draw());
+
+        // Then: No participant should have a partner
+        $this->assertTrue(Participant::all()->every(function ($participant) {
+            return is_null($participant->partner_id);
+        }));
+
+        // Also: No direct messages should have been sent
+        $this->assertCount(0, $service->dmMessages);
+
+        // And: The old announcement post should not have been deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: No new announcement post should have been written
+        $this->assertEmpty($service->message);
+    }
+
     // TODO: When State != DRAWING, don't do anything
     // TODO: When 0 participants, don't do anything
     // TODO: Add config values and .env file
