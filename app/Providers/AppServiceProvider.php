99c83d2300f3e3b6b9b436707e26539d8f1ef87c
Kniest Jordan <Kniest@basecom.de>
2017-11-09 01:10:52 +0100

    Fixed circleci bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale('de_DE');
-        setlocale(LC_TIME, 'de_DE');
+        setlocale(LC_TIME, 'de_DE.UTF-8');
     }
 
     /**
