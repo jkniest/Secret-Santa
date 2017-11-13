c17f323e13b8546dea1fdaa90aeb86aef9275919
Kniest Jordan <Kniest@basecom.de>
2017-11-13 15:45:48 +0100

    Fixed wrong state in stop command and validating date

--- app/Jobs/Stop.php
+++ app/Jobs/Stop.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\Discord\MessageService;
 use App\Models\State;
+use Carbon\Carbon;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -30,11 +31,17 @@ class Stop
      */
     public function handle(MessageService $service)
     {
+        $now = Carbon::now();
+        if ($now->day != 3 || $now->month != 1 || $now->hour != 0 || $now->minute != 0) {
+            return;
+        }
+
         $service->delete(
             State::byName('announcement_id'),
             State::byName('announcement_channel')
         );
 
         State::set('announcement_id', null);
+        State::set('bot', State::STOPPED);
     }
 }
--- tests/Unit/StopTest.php
+++ tests/Unit/StopTest.php
@@ -21,7 +21,7 @@ class StopTest extends TestCase
         Carbon::setTestNow(Carbon::create(Carbon::now()->year, 1, 3, 0));
 
         // Given: The state is set to IDLE
-        State::set('bot', State::STOPPED);
+        State::set('bot', State::IDLE);
 
         // Given: A fakes messaging service
         $service = new FakeMessageService();
@@ -29,7 +29,7 @@ class StopTest extends TestCase
             return $service;
         });
 
-        // Given: The announcement id and channel should be '12345' and '67890'
+        // Given: The announcement id and channel id are '12345' and '67890'
         State::set('announcement_id', '12345');
         State::set('announcement_channel', '67890');
 
@@ -45,12 +45,104 @@ class StopTest extends TestCase
 
         // Also: The announcement id should be set to null
         $this->assertNull(State::byName('announcement_id'));
-
-        // And: The state should have been set to STOPPED
-        $this->assertEquals(State::STOPPED, State::byName('bot'));
     }
 
-    // TODO: Validate date
+    /** @test */
+    public function it_will_only_execute_two_days_after_new_year()
+    {
+        // Given: The date is four days after new-year
+        Carbon::setTestNow(Carbon::create(Carbon::now()->year, 1, 5, 0));
+
+        // Given: The state is set to IDLE
+        State::set('bot', State::IDLE);
+
+        // Given: A fakes messaging service
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement id and channel id are '12345' and '67890'
+        State::set('announcement_id', '12345');
+        State::set('announcement_channel', '67890');
+
+        // When: The stop command is running
+        dispatch(new Stop());
+
+        // Then: The state should not have changed
+        $this->assertEquals(State::IDLE, State::byName('bot'));
+
+        // And: The old announcement message should not be deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: The announcement id should not be null
+        $this->assertNotNull(State::byName('announcement_id'));
+    }
+
+    /** @test */
+    public function it_will_only_execute_if_the_hour_is_zero()
+    {
+        // Given: The date is two days and six hours after new-year
+        Carbon::setTestNow(Carbon::create(Carbon::now()->year, 1, 3, 6));
+
+        // Given: The state is set to IDLE
+        State::set('bot', State::IDLE);
+
+        // Given: A fakes messaging service
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement id and channel id are '12345' and '67890'
+        State::set('announcement_id', '12345');
+        State::set('announcement_channel', '67890');
+
+        // When: The stop command is running
+        dispatch(new Stop());
+
+        // Then: The state should not have changed
+        $this->assertEquals(State::IDLE, State::byName('bot'));
+
+        // And: The old announcement message should not be deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: The announcement id should not be null
+        $this->assertNotNull(State::byName('announcement_id'));
+    }
+
+    /** @testf */
+    public function it_will_only_execute_if_the_minute_is_zero()
+    {
+        // Given: The date is two days and fifteen minutes after new-year
+        Carbon::setTestNow(Carbon::create(Carbon::now()->year, 1, 3, 0, 15));
+
+        // Given: The state is set to IDLE
+        State::set('bot', State::IDLE);
+
+        // Given: A fakes messaging service
+        $service = new FakeMessageService();
+        app()->singleton(MessageService::class, function () use ($service) {
+            return $service;
+        });
+
+        // Given: The announcement id and channel id are '12345' and '67890'
+        State::set('announcement_id', '12345');
+        State::set('announcement_channel', '67890');
+
+        // When: The stop command is running
+        dispatch(new Stop());
+
+        // Then: The state should not have changed
+        $this->assertEquals(State::IDLE, State::byName('bot'));
+
+        // And: The old announcement message should not be deleted
+        $this->assertEmpty($service->deletedPost);
+
+        // Also: The announcement id should not be null
+        $this->assertNotNull(State::byName('announcement_id'));
+    }
+
     // TODO: Validate state
     // TODO: Refactoring
 }
